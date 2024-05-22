b20712443cfc00899b9cf9455f01f57e9adb844c
David Hasenhüttl <hasi1999@gmx.at>
2024-05-22 13:44:55 +0200

    scripts: new usb-keys service

--- scripts/bash/cleardata.sh
+++ scripts/bash/cleardata.sh
@@ -3,14 +3,17 @@
 set -e
 
 rm -f /var/www/node/passkeys/myDataBase.json
+rm -f /var/www/node/usb-keys/myDataBase.json
+rm -f /var/www/node/facial-recognition-api/uploads/img1.jpg
+rm -f /var/www/node/facial-recognition-api/uploads/img2.jpg
+
 
 systemctl daemon-reload
 systemctl restart passkeys
+systemctl restart usb-keys
 systemctl restart facial-recognition-api
 #systemctl restart scripts
 #systemctl restart nginx
 
 
-rm /var/www/node/facial-recognition-api/uploads/img1.jpg
-rm /var/www/node/facial-recognition-api/uploads/img2.jpg
 
