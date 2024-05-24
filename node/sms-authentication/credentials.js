2a2c972fdb5999ab8db9e7a2f088b5b63c7cf55b
David Hasenhüttl <hasi1999@gmx.at>
2024-05-24 14:30:15 +0200

    sms-authentication remove credentials.js

--- node/sms-authentication/credentials.js
+++
@@ -1,4 +0,0 @@
-var fromNumber = 'your_twilio_phone_number'; // Your Twilio phone number
-const accountSid = 'your_account_sid'; // Your Account SID from www.twilio.com/console
-const authToken = 'your_auth_token';   // Your Auth Token from www.twilio.com/console
-
