a2d58d8026349fcab565837242f4af1a4af544a3
David Hasenhüttl <hasi1999@gmx.at>
2024-05-24 14:09:18 +0200

    sms-authentication twilio credentials

---
+++ node/sms-authentication/credentials.js
@@ -0,0 +1,4 @@
+const fromNumber = 'your_twilio_phone_number'; // Your Twilio phone number
+const accountSid = 'your_account_sid'; // Your Account SID from www.twilio.com/console
+const authToken = 'your_auth_token';   // Your Auth Token from www.twilio.com/console
+
