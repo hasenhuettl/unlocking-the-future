ec537eddb792f6e9dee32cbe0fee843c2bbc0bbd
David Hasenhüttl <hasi1999@gmx.at>
2024-06-03 15:11:42 +0200

    passwords: Improve error message passwords (what is wrong: special character, etc.)

--- html/password-authentication/js/password-authentication.js
+++ html/password-authentication/js/password-authentication.js
@@ -1,8 +1,8 @@
 const apiUrl = 'https://authenticate.hasenhuettl.cc/password-authentication-api';
 
 async function login() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const username = $("#username").val();
+    const password = $("#password").val();
     try {
         const response = await fetch(apiUrl + '/login', {
             method: 'POST',
@@ -21,8 +21,8 @@ async function login() {
 }
 
 async function signup() {
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
+    const username = $("#username").val();
+    const password = $("#password").val();
     try {
         const response = await fetch(apiUrl + '/signup', {
             method: 'POST',
@@ -41,8 +41,30 @@ async function signup() {
     }
 }
 
+function validatePassword() {
+  const password = $("#password").val();
+  const requirements = {
+      characters: { element: $('#requirement-characters'), regex: /.{10,}/ },
+      lowercase: { element: $('#requirement-lowercase'), regex: /[a-z]/ },
+      uppercase: { element: $('#requirement-uppercase'), regex: /[A-Z]/ },
+      number: { element: $('#requirement-number'), regex: /\d/ },
+      special: { element: $('#requirement-special'), regex: /[^A-Za-z0-9]/ }
+  };
+  for (const key in requirements) {
+    const requirement = requirements[key];
+    console.log(requirement.element.text());
+    if (requirement.regex.test(password)) {
+      requirement.element.attr('status', 'success');
+    } else {
+      requirement.element.attr('status', 'fail');
+    }
+  }
+}
+
+
 window.onload = function(){
   $("#login").on("click", function(){ login(); });
   $("#signup").on("click", function(){ signup(); });
+  $("#password").on('change paste keyup input', function() { validatePassword() })
 }
 
