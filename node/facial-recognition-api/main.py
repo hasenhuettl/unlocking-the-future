d9d39cfe04682504ecbf645ee54f3a0dfc972a07
David Hasenhüttl <hasi1999@gmx.at>
2024-06-04 13:57:19 +0200

    facial-recognition-api: remove result.txt

--- node/facial-recognition-api/main.py
+++ node/facial-recognition-api/main.py
@@ -2,6 +2,10 @@
 from deepface import DeepFace
 import os
 import sys
+import time
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 def get_script_path():
     return os.path.dirname(os.path.realpath(sys.argv[0]))
@@ -12,11 +16,7 @@ img2_path = get_script_path() + "/uploads/img2.jpg"
 #verification = DeepFace.verify(img1_path = img1_path, img2_path = img2_path)
 result = DeepFace.verify(img1_path, img2_path, model_name="VGG-Face", detector_backend="opencv", distance_metric="cosine", enforce_detection=True, align=True, normalization="base")
 
-f = open("result.txt", "w")
-
 verified = str(result['verified'])
 
-f.write(verified)
-f.close()
-
 print(verified)
+
