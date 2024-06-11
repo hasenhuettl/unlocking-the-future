dba9827643b16404ab8a078f6b93b9211ba6036d
David Hasenhüttl <hasi1999@gmx.at>
2024-06-11 16:58:13 +0200

    gps-verification: add allowed locations

--- html/gps-verification/js/gps-verification.js
+++ html/gps-verification/js/gps-verification.js
@@ -1,45 +1,55 @@
 window.onload = function(){
+  $("#signup").on("click", function(){ signup() });
+  $("#login").on("click", function(){ login() });
+}
+
+async function getCoords() {
+  return new Promise((resolve, reject) => {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(handleLocationSuccess, handleLocationError);
+      navigator.geolocation.getCurrentPosition(position => {
+        const coords = {
+          lat: position.coords.latitude.toFixed(3),
+          lon: position.coords.longitude.toFixed(3)
+        };
+        resolve(coords);
+      }, reject);
     } else {
-        console.error('Geolocation is not supported by this browser.');
-        window.location.href = '/gps-verification/403.html';
+      reject(new Error("Geolocation is not supported by this browser."));
     }
+  });
 }
 
-async function checkLocationWithAPI(latitude, longitude) {
-    try {
-        const response = await fetch(`/gps-verification/api?lat=${latitude}&lon=${longitude}`);
-        if (!response.ok) {
-            throw new Error('Network response was not ok ' + response.statusText);
-        }
-        const data = await response.json();
-        const results = data.features;
-        if (results.length > 0) {
-            const country = results[0].properties.country_code;
-            return country && country.toUpperCase() === 'AT'; // 'AT' is the ISO country code for Austria
-        }
-        return false;
-    } catch (error) {
-        console.error('Error fetching location data:', error);
-        return false;
-    }
+async function signup() {
+  try {
+    const coords = await getCoords();
+    const response = await fetch('/gps-verification-api/signup', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(coords)
+    });
+    const result = await response.json();
+    window.location.href = "/success";
+  } catch (error) {
+    showError(error.message);
+  }
 }
 
-async function handleLocationSuccess(position) {
-    const latitude = position.coords.latitude;
-    const longitude = position.coords.longitude;
-
-    const inAustria = await checkLocationWithAPI(latitude, longitude);
-    if (inAustria) {
-        window.location.href = '/success';
-    } else {
-        window.location.href = '/gps-verification/403.html';
-    }
-}
-
-function handleLocationError(error) {
-    console.error('Error getting location:', error);
-    window.location.href = '/gps-verification/403.html';
+async function login() {
+  try {
+    const coords = await getCoords();
+    const response = await fetch('/gps-verification-api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(coords)
+    });
+    const result = await response.json();
+    window.location.href = "/success";
+  } catch (error) {
+    showError(error.message);
+  }
 }
 
