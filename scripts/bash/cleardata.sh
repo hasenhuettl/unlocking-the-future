4b6f7df2db0f88644276b8d7ae424cfee6583b43
David Hasenhüttl <hasi1999@gmx.at>
2024-06-21 15:28:11 +0200

    update bash script

--- scripts/bash/cleardata.sh
+++ scripts/bash/cleardata.sh
@@ -3,21 +3,28 @@
 set -e
 
 rm -f /var/www/node/passkeys/.data/db.json
-rm -f /var/www/node/usb-keys/myDataBase.json
+rm -f /var/www/node/usb-keys/.data/db.json
+rm -f /var/www/node/smart-cards/.data/db.json
 rm -f /var/www/node/password-authentication-api/myDatabase.json
 rm -f /var/www/node/pin-authentication-api/myDatabase.json
 rm -f /var/www/node/security-question-authentication-api/myDatabase.json
 rm -f /var/www/node/sms-authentication-api/myDatabase.json
 rm -f /var/www/node/device-fingerprint-api/myDatabase.json
 rm -f /var/www/node/gps-verification-api/myDatabase.json
+rm -f /var/www/node/authenticator-apps-api/myDatabase.json
 
-rm -f /var/www/node/facial-recognition-api/uploads/img1.jpg
-rm -f /var/www/node/facial-recognition-api/uploads/img2.jpg
+rm -f /var/www/node/facial-recognition-api/uploads/*
+rm -f /var/www/node/voice-authentication-api/uploads/*
+rm -f /var/www/node/voice-authentication-api/models/*
 
+touch /var/www/node/facial-recognition-api/uploads/.gitkeep
+touch /var/www/node/voice-authentication-api/uploads/.gitkeep
+touch /var/www/node/voice-authentication-api/models/.gitkeep
 
 systemctl daemon-reload
 systemctl restart passkeys
 systemctl restart usb-keys
+systemctl restart smart-cards
 systemctl restart password-authentication
 systemctl restart pin-authentication
 systemctl restart security-question-authentication
@@ -26,6 +33,9 @@ systemctl restart sms-authentication
 systemctl restart device-fingerprint
 systemctl restart ip-address-filtering
 systemctl restart gps-verification
+systemctl restart authenticator-apps
+systemctl restart voice-authentication
+systemctl restart voice-authentication-python
 #systemctl restart scripts
 #systemctl restart nginx
 
