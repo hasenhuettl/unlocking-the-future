6f073c9c77505ec1fcd77cde09c3b9927ad8c75f
David Hasenhüttl <hasi1999@gmx.at>
2024-08-14 17:53:39 +0200

    Node: Axios security update

--- setup_postgres.sh
+++ setup_postgres.sh
@@ -11,6 +11,6 @@ source ${__dir}/.credentials.env
 sudo -u postgres psql -c "CREATE DATABASE $database_name;"
 sudo -u postgres psql -c "ALTER USER postgres with encrypted password '$POSTGRES';"
 #sudo -u postgres psql -c "CREATE USER authenticate with encrypted password '$POSTGRES';"
-# sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $database_name TO authenticate;"
+#sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE $database_name TO authenticate;"
 sudo -u postgres psql -d $database_name -f ${__dir}/schema.sql
 
