e8ab4fe8376668281c0a1191d0f63f3b8bb178f2
David Hasenhüttl <hasi1999@gmx.at>
2024-09-24 13:16:27 +0200

    scripts: count-line-numbers - better visual display of data

--- html/count-line-numbers/js/count-line-numbers.js
+++ html/count-line-numbers/js/count-line-numbers.js
@@ -1,10 +1,14 @@
 $( document ).ready(function() {
 
+  showLoad();
+
   $.ajax({
     url: "/scripts/countLineNumbers"
   }).done(function(data){
+    showMain();
     $('#data').text(data);
   }).fail(function(error){
+    showMain();
     showError(error);
   });
 
--- scripts/bash/count_line_numbers.sh
+++ scripts/bash/count_line_numbers.sh
@@ -15,26 +15,10 @@ SCRIPTS_DIR="$PROJECT_ROOT/scripts"
 SYSTEMD_DIR="$PROJECT_ROOT/systemd"
 SETUP_DIR="$PROJECT_ROOT/setup"
 
-count_files() {
-    local dir=$1
-    local lang=$2
-    local file_count=0
-
-    if [ -d "$dir" ]; then
-        for file in $(git ls-files "$dir" | grep -E "$lang$"); do
-            if [ -f "$file" ]; then
-                file_count=$((file_count + 1))
-            fi
-        done
-    fi
-
-    echo "$file_count"
-}
-
-# Function to count lines of specific file extensions in a directory
 count_lines() {
     local dir=$1
     local lang=$2
+    local file_count=0
     local total_lines=0
 
     if [ -d "$dir" ]; then
@@ -42,11 +26,12 @@ count_lines() {
             if [ -f "$file" ]; then
                 lines=$(wc -l < "$file")
                 total_lines=$((total_lines + lines))
+                file_count=$((file_count + 1))
             fi
         done
     fi
 
-    echo "$total_lines"
+    echo "$total_lines ($file_count Files)"
 }
 
 # Count lines for different file types in the respective directories
@@ -54,39 +39,43 @@ count_lines() {
 echo "Displaying current line numbers in project"
 
 echo ""
-echo 'Folder "html":'
+echo "Folder "html":"
 echo "  html:   $(count_lines "$HTML_DIR" "html")"
 echo "  js:     $(count_lines "$HTML_DIR" "js")"
 echo "  css:    $(count_lines "$HTML_DIR" "css")"
+echo ""
+echo "  ALL:    $(count_lines "$HTML_DIR" "(html|js|css|sh|service)")"
 
 echo ""
-echo 'Folder "node":'
+echo "Folder "node":"
 echo "  html:   $(count_lines "$NODE_DIR" "html")"
 echo "  js:     $(count_lines "$NODE_DIR" "js")"
 echo "  css:    $(count_lines "$NODE_DIR" "css")"
+echo ""
+echo "  ALL:    $(count_lines "$NODE_DIR" "(html|js|css|sh|service)")"
 
 echo ""
-echo 'Folder "scripts":'
+echo "Folder "scripts":"
 echo "  sh:     $(count_lines "$SCRIPTS_DIR" "sh")"
 
 echo ""
-echo 'Folder "setup":'
+echo "Folder "setup":"
 echo "  sh:     $(count_lines "$SETUP_DIR" "sh")"
 
 echo ""
-echo 'Folder "systemd":'
+echo "Folder "systemd":"
 echo "  service: $(count_lines "$SYSTEMD_DIR" "service")"
 
 echo ""
 echo "----------------------------------"
 echo ""
-echo 'All Folders:'
-echo "  html:    $(count_lines "$PROJECT_ROOT" "html")	($(count_files "$PROJECT_ROOT" "html") Files)"
-echo "  js:      $(count_lines "$PROJECT_ROOT" "js")	($(count_files "$PROJECT_ROOT" "js") Files)"
-echo "  css:     $(count_lines "$PROJECT_ROOT" "css")	($(count_files "$PROJECT_ROOT" "css") Files)"
-echo "  sh:      $(count_lines "$PROJECT_ROOT" "sh")	($(count_files "$PROJECT_ROOT" "sh") Files)"
-echo "  service: $(count_lines "$PROJECT_ROOT" "service")	($(count_files "$PROJECT_ROOT" "service") Files)"
+echo "All Folders:"
+echo "  html:    $(count_lines "$PROJECT_ROOT" "html")"
+echo "  js:      $(count_lines "$PROJECT_ROOT" "js")"
+echo "  css:     $(count_lines "$PROJECT_ROOT" "css")"
+echo "  sh:      $(count_lines "$PROJECT_ROOT" "sh")"
+echo "  service: $(count_lines "$PROJECT_ROOT" "service")"
 
 echo ""
-echo "  ALL:    $(count_lines "$PROJECT_ROOT" "(html|js|css|sh|service)") ($(count_files "$PROJECT_ROOT" "(html|js|css|sh|service)") Files)"
+echo "  ALL:    $(count_lines "$PROJECT_ROOT" "(html|js|css|sh|service)")"
 
